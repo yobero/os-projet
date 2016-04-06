466934cf77aed1ba9c874df9c8106638c015e4ed
yobero <yoannb94@gmail.cam>
2016-04-06 20:27:55 +0200

    Ajout du fichier pour le processus directeur

---
+++ principal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <pthread.h>
+#include <semaphore.h>
+
+#include <time.h>
+
+/** n processus  ==> n fichier en entrée
+ *  m threads
+ * */
+
+int main(int argv, char** argc)
+{
+	
+	
+	return 1;
+}
